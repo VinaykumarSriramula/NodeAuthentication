429f1dab962634b31f7460f13e9f27c22cdae428
sriramulavinaykumar <vinaysriramula22@gmail.com>
2019-10-28 09:44:44 +0530

    delete operation

--- src/routers/userRoutes.js
+++ src/routers/userRoutes.js
@@ -71,7 +71,25 @@ router.post('/users/login', async (req, res) => {
         res.status(500).send(e);
     }
 })
-
+router.patch('/users/me', auth ,async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['name','email', 'password', 'age'];
+    const isValidOperation = updates.every((update)=> {
+        allowedUpdates.includes(update);
+    })
+    if(!isValidOperation){
+        res.status(500).send('Please update Valid Key')
+    }
+    try{
+        updates.forEach((update) => {
+            req.user[update] = req.body[update];
+        })
+        await req.user.save();
+        res.status(200).send(req.user)
+    }catch(e){
+        res.status(400).send('Unable to update User');
+    }
+})
 router.get('/users/:id', (req, res) => {
     const _id = req.params.id;
 
@@ -83,8 +101,16 @@ router.get('/users/:id', (req, res) => {
     })
 })
 
-router.delete('/user/:id', async (req, res) => {
-    res.status(200).send("SuccessFully Done");
+router.delete('/user/me', auth, async (req, res) => {
+    console.log("delete profile");
+    try{
+    await req.user.remove();
+    res.send(req.user);
+    }catch(e){
+        res.status(500).send('unable to delete profile')
+    }
+    // const user = await User.findByIdAndDelete(req.user._id);
+    // res.status(200).send("SuccessFully Deleted");
 });
 
 module.exports = router;
