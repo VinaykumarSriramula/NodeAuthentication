ec5ca7af23351dc39a10cba59957c6672f3f4925
Sriramula Vinay kumar <vinaysriramula22@gmail.com>
2019-10-29 09:52:41 +0530

    Update mongoose.js
    mongoose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
     // useNewUrlParser : true,
     // useCreateIndex : true,
-    // useUnifiedTopology : true
+    useUnifiedTopology : true
 })
